7a469d0b966f237e9e2029c548fba7e05ac22804
Riya Joshi <60728693+riya2820@users.noreply.github.com>
2024-01-21 19:50:00 -0800

    initial commit
    imported libraries

---
+++ model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(data)
+total_words = len(tokenizer.word_index) + 1
+
+input_sequences = []
+for line in data:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+
+max_sequence_length = max([len(x) for x in input_sequences])
+input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
